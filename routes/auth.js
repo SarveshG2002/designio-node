868d553c85294a47622e373c77a67584500e4b3b
Sarvesh Gandhere <sarveshgandhere2002@gmail.com>
2023-11-20 23:21:34 +0530

    register page created

--- routes/auth.js
+++ routes/auth.js
@@ -9,10 +9,18 @@ router.get('/login', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/view/login.html'));
 });
 
+router.post('/login', (req, res) => {
+    // res.sendFile(path.join(__dirname, '../public/view/login.html'));
+    res.send("login succces");
+});
+
 // Define a route for user registration
-// router.get('/register', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'public', 'view', 'register.html'));
-// });
+router.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/view/register.html'));
+});
+router.post('/register', (req, res) => {
+    res.send("register succces");
+  });
 
 // Add more routes as needed for authentication
 
