c7d1ebdc966acf9bb4e9fc34e68887c435ef7915
Sarvesh Gandhere <sarveshgandhere2002@gmail.com>
2023-12-10 17:53:43 +0530

    unfollow function in controller done

--- controllers/FriendController.js
+++ controllers/FriendController.js
@@ -2,19 +2,33 @@ const Follower = require('../models/Friend.js'); // Make sure to adjust the path
 const fs = require('fs').promises;
 
 
-async function followfriend(req,res){
-    try{
-        const Friend = new Follower({
+async function followOrUnfollow(req, res) {
+    try {
+        const { id } = req.body;
+        const followerEntry = await Follower.findOne({
             user_id: req.session.id,
-            followed_user_id: req.body.id,
+            followed_user_id: id,
         });
-        await Friend.save();
-        res.json({message:"Followed successfully",session:req.session});
-    }catch(error){
+
+        if (followerEntry) {
+            // If the entry exists, delete it (unfollow)
+            await Follower.findByIdAndDelete(followerEntry._id);
+            res.json({ message: "Unfollowed successfully", session: req.session });
+        } else {
+            // If the entry does not exist, create it (follow)
+            const newFollower = new Follower({
+                user_id: req.session.id,
+                followed_user_id: id,
+            });
+            await newFollower.save();
+            res.json({ message: "Followed successfully", session: req.session });
+        }
+    } catch (error) {
+        console.error('Error following/unfollowing friend:', error);
         res.status(500).send({ error: 'Internal Server Error' });
     }
 }
 
 module.exports = {
-    followfriend,
+    followOrUnfollow,
 };
--- routes/friends.js
+++ routes/friends.js
@@ -2,9 +2,7 @@ const express = require('express');
 const router = express.Router();
 const friendController = require('../controllers/FriendController');
 
-router.post('/followfriend', friendController.followfriend);
-
-
+router.post('/followfriend', friendController.followOrUnfollow);
 
 
 module.exports = router;
